7b29fd6b022c82e0a51ded4a235938e5d36ca5db
Daniel Kelly <me@danielkelly.io>
2021-11-17 16:22:35 -0600

    install eslint and prettier

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
 </script>
 
 <template>
-  <ul class="flex mb-10 gap-4 bg-gray-500 p-5 text-white">
-    <li><router-link :to="{name:'Home'}">Home</router-link></li>
-    <li><router-link :to="{name:'About'}">About</router-link></li>
-  </ul>
+  <nav class="flex mb-10 gap-4 bg-gray-500 p-5 text-white">
+    <router-link :to="{ name: 'Home' }"> Home </router-link>
+    <router-link :to="{ name: 'About' }"> About </router-link>
+  </nav>
   <div class="max-w-5xl m-auto">
-    <router-view/>
+    <router-view />
   </div>
 </template>
