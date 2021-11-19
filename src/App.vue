07d3deb6f154b564833829af34ce3daf91f1a543
Daniel Kelly <me@danielkelly.io>
2021-11-19 14:30:38 -0600

    jsconfig and AppHeader component to keep components dir in repo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import AppNav from "@/components/AppNav.vue";
 </script>
 
 <template>
-  <nav class="flex mb-10 gap-4 bg-gray-500 p-5 text-white">
-    <router-link :to="{ name: 'Home' }"> Home </router-link>
-    <router-link :to="{ name: 'About' }"> About </router-link>
-  </nav>
+  <AppNav />
   <div class="max-w-5xl m-auto">
     <router-view />
   </div>
